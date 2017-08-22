e2c8c7df3b85a8e0406580437b087684271add2a
何全 <hequan2011@sina.com>
2017-08-22 17:31:10 +0800

    2.3
    2.3

--- install_webssh.sh
+++ install_webssh.sh
@@ -16,8 +16,8 @@ GOPATH=/opt/webconsole go install
 
 
 #  "addr": ":9000",  ##修改端口为9000  可以自定义。 我设置的为9000，如果修改成别的端口，需要修改网页。 templates/host/host.html
-#"enable_jsonp": true,    开启jsonp，启用跨域访问
-#"cors_white_list": "42.62.6.54,42.62.6.54:9000,,  这里输入的地址为需要跨域访问的，添加web主机的地址。
+# "enable_jsonp": true,    开启jsonp，启用跨域访问
+# "cors_white_list": "42.62.6.54,42.62.6.54:9000,"  这里输入的地址为需要跨域访问的，添加web主机的地址。
 
 
 
